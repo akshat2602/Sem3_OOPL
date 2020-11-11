97008ac998434d1eee3c75fa2d7249e20a1d2630
akshat2602 <akshatsharma2602@gmail.com>
2020-11-11 15:26:01 +0530

    Added menu to 4th assignment

--- Assignment_4.cpp
+++ Assignment_4.cpp
@@ -4,24 +4,43 @@ using namespace std;
 
 int main() 
 { 
-    ofstream fout;
+    bool flag = true;
+    int resp;
     string s; 
-    fout.open("sample.txt", ios::app); 
-    while (fout) { 
-        getline(cin, s); 
-        if (s == "/") 
-            break; 
+    do
+    {
+        cout << "1) Read from a file \n2) Write to a file \n3) Exit" << endl;
+        cin >> resp;
+        if (resp == 1)
+        {
+            ofstream fout;
+            fout.open("sample.txt", ios::app); 
+            while (fout) 
+            { 
+                getline(cin, s); 
+                if (s == "/") 
+                    break; 
    
-        fout << s << endl; 
-    } 
-    fout.close(); 
-    // string s;
-    ifstream fin; 
-    fin.open("sample.txt"); 
-    while (fin) { 
-        getline(fin, s); 
-        cout << s << endl; 
-    } 
-    fin.close(); 
+                fout << s << endl; 
+                fout.close(); 
+            } 
+        }
+        else if (resp == 2)
+        {
+            ifstream fin; 
+            fin.open("sample.txt"); 
+            while (fin) 
+            { 
+                getline(fin, s); 
+                cout << s << endl; 
+            } 
+            fin.close(); 
+        }
+        else if (resp == 3)
+        {
+            flag == false;
+        }
+    } while (flag == true);
+    
     return 0; 
 } 
